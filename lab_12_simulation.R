76bf3d484dac647b12d01ccea406a248abe284b8
User <shlok.goyal@gmail.com>
2018-04-17 13:41:28 -0400

    Added a function to return p-values of most significant covariates of a given regression.

--- lab_12_simulation.R
+++ lab_12_simulation.R
@@ -4,3 +4,13 @@ generate_data = function(n, p){
   
   return(list(covariates = covariates, responses = responses))
 }
+
+model_select = function(covariates, responses, cutoff){
+  linreg = lm(responses ~ covariates)
+  p_vals = which(unname(summary(linreg)$coefficients[,4] <= cutoff))
+  p_vals = p_vals[p_vals > 1]
+  p_vals = p_vals - 1
+  if(length(p_vals) > 0){
+    return(summary(lm(responses ~ covariates[,p_vals]))$coefficients[,4])
+  } else { return(p_vals)}
+}
