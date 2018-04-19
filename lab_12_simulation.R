52c79be6d8b3eede85512754a753f9182e4cd649
User <shlok.goyal@gmail.com>
2018-04-19 00:38:40 -0400

    Added a function ro run simulations and tested function on 9 inputs.

--- lab_12_simulation.R
+++ lab_12_simulation.R
@@ -14,3 +14,22 @@ model_select = function(covariates, responses, cutoff){
     return(summary(lm(responses ~ covariates[,p_vals]))$coefficients[,4])
   } else { return(p_vals)}
 }
+
+run_simulation = function(n_trials, n, p, cutoff){
+  p_values = c()
+  
+  for (i in 1:n_trials){
+  generator = generate_data(n,p)
+  selection = model_select(generator[["covariates"]], generator[["responses"]], cutoff)
+  p_values = c(p_values, selection)
+  }
+  
+  hist(p_values)
+}
+
+ner = c(100, 1000, 10000)
+for (i in ner){
+  run_simulation(100, i, 10, 0.05)
+  run_simulation(100, i, 20, 0.05)
+  run_simulation(100, i, 50, 0.05)
+}
